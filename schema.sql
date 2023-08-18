108870bf20525f0432cb4d7c08253dd694db88a0
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-08-18 08:02:53 +0200

    Create more tables

--- schema.sql
+++ schema.sql
@@ -11,5 +11,31 @@ CREATE TABLE animals (
 ALTER TABLE
     animals
 ADD
-    COLUMN species VARCHAR(255)
+    COLUMN species VARCHAR(255) -- Query multiple tables
+    CREATE TABLE owners (
+        id SERIAL PRIMARY KEY,
+        full_name VARCHAR(255),
+        age INTEGER
+    );
 
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+-- Modify animals table 
+-- Remove the species column
+ALTER TABLE
+    animals DROP COLUMN species;
+
+-- Add the species_id column as a foreign key referencing species table
+ALTER TABLE
+    animals
+ADD
+    COLUMN species_id INTEGER REFERENCES species(id);
+
+-- Add the owner_id column as a foreign key referencing owners table
+ALTER TABLE
+    animals
+ADD
+    COLUMN owner_id INTEGER REFERENCES owners(id);
